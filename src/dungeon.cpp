e460020e4abb93d6a55cd884148a1b306cc4dede
sibjor <sixteen610@gmail.com>
2025-03-06 23:51:07 +0100

    I hope I will manage to not draw only symmetrical rooms by this algorithm

--- src/dungeon.cpp
+++ src/dungeon.cpp
@@ -1,3 +1,44 @@
 #include "dungeon.h"
 #include "random.h"
 
+struct Tile
+{
+    char type;
+    int posX;
+    int posY;
+};
+
+FirstFloor::FirstFloor(){
+    // Constructor
+    roomCount = random(5, 10);
+    for (int i = 0; i < roomCount; i++)
+    {
+        Room room = Room();
+        rooms.push_back(room);
+    }
+}
+
+Dungeon::Dungeon(){
+    // Constructor
+    roomCount = random(5, 10);
+    for (int i = 0; i < roomCount; i++)
+    {
+        Room room = Room();
+        rooms.push_back(room);
+    }
+}
+
+Room::Room(){
+    // Constructor
+    int value = random(5,10);
+    for (int i = 0; i < value; i++)
+    {
+        for (int j = 0; j < value; j++)
+        {
+            Tile tile = Tile();
+            tile.posX = i;
+            tile.posY = j;
+            tiles[i][j] = tile;
+        }
+    }
+}
