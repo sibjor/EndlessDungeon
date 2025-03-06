6c327a258319141728720008e26a516caed7c1b9
sibjor <sixteen610@gmail.com>
2025-03-06 23:04:01 +0100

    Thinking about changing from BSP to my own Tile based generator

--- src/dungeon.cpp
+++ src/dungeon.cpp
@@ -1,19 +1,3 @@
 #include "dungeon.h"
 #include "random.h"
 
-
-Dungeon::Dungeon(int timesDivided) {
-    // Constructor implementation
-    width = DUNGEON_WIDTH;
-    height = DUNGEON_HEIGHT;
-    this->timesDivided = timesDivided;
-    
-}
-
-Dungeon::~Dungeon() {
-    // Destructor implementation
-}
-Room::~Room() {
-    // Destructor implementation
-}
-
